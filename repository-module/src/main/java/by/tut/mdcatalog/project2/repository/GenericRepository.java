0a871f5dc09f08e1512f5ac5a55c5e4bc7364bb7
S K <S K>
2019-05-13 02:06:11 +0300

    Datasource and liquibase for DB added

---
+++ repository-module/src/main/java/by/tut/mdcatalog/project2/repository/GenericRepository.java
@@ -0,0 +1,8 @@
+package by.tut.mdcatalog.project2.repository;
+
+import java.sql.Connection;
+
+public interface GenericRepository {
+
+    Connection getConnection();
+}
--- repository-module/src/main/java/by/tut/mdcatalog/project2/repository/connection/ConnectionService.java
+++
@@ -1,8 +0,0 @@
-package by.tut.mdcatalog.project2.repository.connection;
-
-import java.sql.Connection;
-
-public interface ConnectionService {
-
-    Connection getConnection();
-}
